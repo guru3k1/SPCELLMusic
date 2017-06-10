5dd95e3a25596dcaf6e491839ea76282cee5120b
Cesar Amadori <cesargamadori@gmail.com>
2017-06-10 17:50:49 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appName:'spcellmusic',
+	apiKey:'32292f1d78b8e5ab8efc6bc03a2f64ac',
+	secret:'6709d8cfbb4b5ff7612997c9863753df',
+	registeredTo:'cesargamadori'
+}
+
+
+//Application name	spcellmusic
+//API key	32292f1d78b8e5ab8efc6bc03a2f64ac
+//Shared secret	6709d8cfbb4b5ff7612997c9863753df
+//Registered to	cesargamadori
